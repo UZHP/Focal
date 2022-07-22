57a301105d2243fb6f50f7547b23ef1d5d6bc083
Youssef Tamer <107003202+UZHP@users.noreply.github.com>
2022-07-23 01:27:42 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,14 @@ window.addEventListener("load", vanish);
 function vanish() {
     loader.classList.add("disappear")
 }
+
+var button = document.querySelector(".list");
+var list = document.querySelector(".navHover")
+
+button.onclick = function () {
+    if (list.className == "navHover") {
+        list.classList.add("open");
+    } else {
+        list.classList.remove("open")
+    }
+}
